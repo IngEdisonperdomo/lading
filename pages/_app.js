c8f7a826dd436dbb5bcf8fb1738f036ccc00b6cb
Edison <edison@ucrop.it>
2021-10-07 11:33:26 -0400

    feat: lading page with tailwind and nextjs

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,24 @@
-import '../styles/globals.css'
+import "../styles/index.css";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <meta
+          name="viewport"
+          content="initial-scale=1.0, width=device-width"
+        ></meta>
+        <title>Landing page in nextjs</title>
+        <meta
+          key="description"
+          name="description"
+          content="lading page in nextjs"
+        ></meta>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
